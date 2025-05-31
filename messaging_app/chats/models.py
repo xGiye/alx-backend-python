397cc7e93e6145556b2c2e062f6238ad1e836fb3
Onengiye <onengiyeamadi@gmail.com>
2025-05-31 16:49:20 +0100

    Models and serializer

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    """
+        An extension of the Abstract user for valves not defined in django
+    """
+    bio = models.TextField(blank=True, null=True)
+    phone_number = models.CharField(max_length=11,blank=True,null=True)
+    
+    
+    
+class Conversation(models.Model):
+    
+    """
+        Coversation model that tracks which user are involved in a conversion
+    """
+    participants = models.ManyToManyField(CustomUser, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    """
+        Model model containing the sender, converation
+    """
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='messages')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import User, Conversation, Message
+
+
+# User Serializer
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number']
+
+
+# Message Serializer
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['message_id', 'sender', 'conversation', 'message_body', 'sent_at', 'created_at']
+
+
+# Conversation Serializer
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants', 'messages', 'created_at']
