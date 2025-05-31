1081401320b74f7448c7b95141078eea42c92a63
Onengiye <onengiyeamadi@gmail.com>
2025-05-31 17:05:06 +0100

    fix serializer charfield

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,16 +3,16 @@ from .models import User, Conversation, Message
 
 
 # User Serializer
-
 class UserSerializer(serializers.ModelSerializer):
+    phone_number = serializers.CharField()  
+
     class Meta:
         model = User
         fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number']
-        extrs_kwargs = {'password': {'write_only':True}}
+        extra_kwargs = {'password': {'write_only': True}}  
 
 
 # Message Serializer
-
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
 
@@ -24,18 +24,17 @@ class MessageSerializer(serializers.ModelSerializer):
         if not value.strip():
             raise serializers.ValidationError("Message body cannot be empty.")
         return value
-    
-    
-# Conversation Serializer
 
+
+# Conversation Serializer
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
-    messages = serializers.SerializerMethodField()  # SerializerMethodField usage
+    messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants', 'created_at', 'messages']
 
     def get_messages(self, obj):
-        messages = obj.messages.all()  # reverse relation from Message model
+        messages = obj.messages.all()
         return MessageSerializer(messages, many=True).data
