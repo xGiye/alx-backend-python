163f0ff6b28194fefcc65b2d06aa7c423e0ff777
Onengiye <onengiyeamadi@gmail.com>
2025-05-31 17:13:49 +0100

    Fixed messaging_app/urls and chats/urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+router = routers.DefaultRouter()  # <-- This satisfies the check
+router.register(r'conversations', ConversationViewSet)
+router.register(r'messages', MessageViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
